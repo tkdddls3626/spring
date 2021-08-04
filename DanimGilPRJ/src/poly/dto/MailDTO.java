64a8d62d030cae036de2314a0d25e3f7734e4025
tkddls3626 <tkddls3626@naver.com>
2021-08-04 11:33:45 +0900

    MailDTO

---
+++ DanimGilPRJ/src/poly/dto/MailDTO.java
@@ -0,0 +1,28 @@
+package poly.dto;
+
+public class MailDTO {
+private String toMail;	 //받는사람
+private String title;//보내는 메일 제목
+private String contents; // 보내는 메일 내용
+public String getToMail() {
+	return toMail;
+}
+public void setToMail(String toMail) {
+	this.toMail = toMail;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getContents() {
+	return contents;
+}
+public void setContents(String contents) {
+	this.contents = contents;
+}
+
+	
+	}
+	
