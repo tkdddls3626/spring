dc2318cffb983b2a9be6bc8ade96c75782ce1437
tkddls3626 <tkddls3626@naver.com>
2021-09-04 11:23:42 +0900

    MonitorController

---
+++ DanimGilPRJ/src/poly/controller/MonitorController.java
@@ -0,0 +1,12 @@
+package kopo.poly.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class MonitorController {
+
+
+
+}
