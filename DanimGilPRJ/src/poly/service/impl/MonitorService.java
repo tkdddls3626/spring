95d37fcb05c67a9556ea2afee75599922a0d7e32
tkddls3626 <tkddls3626@naver.com>
2021-09-16 13:34:38 +0900

    MonitorService

---
+++ DanimGilPRJ/src/poly/service/impl/MonitorService.java
@@ -0,0 +1,10 @@
+package kopo.poly.service.impl;
+
+import kopo.poly.service.IMonitorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service("MonitorService")
+public class MonitorService implements IMonitorService {
+}
